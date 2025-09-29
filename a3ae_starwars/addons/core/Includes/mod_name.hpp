aa391648e45f1cbc223e57e2e88af6b0f3540e9f
Singijeonn <holamanpreguntas@gmail.com>
2025-09-29 02:01:08 -0300

    initial setup

---
+++ a3ae_starwars/addons/core/Includes/mod_name.hpp
@@ -0,0 +1,4 @@
+#define MODFOLDER a3ae_starwars
+#ifndef PREFIX
+    #define PREFIX a3ae_starwars
+#endif
---
+++ a3ae_starwars/addons/functions/script_component.hpp
@@ -0,0 +1,2 @@
+#define COMPONENT functions
+#include "\x\a3ae_starwars\addons\core\Includes\script_mod.hpp"
