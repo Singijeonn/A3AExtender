ef04e9aa900887e532b6a652b5877933337af3be
Singijeonn <holamanpreguntas@gmail.com>
2025-10-01 18:26:08 -0300

    Update mod.cpp

--- a3ae_starwars/mod.cpp
+++ a3ae_starwars/mod.cpp
@@ -1,5 +1,5 @@
-name = "A3 Antistasi Extender example";
-author = "A3-Antistasi dev team";
-tooltip = "Antistasi Extender example";
+name = "A3 Antistasi Star Wars Extender";
+author = "Singijeon";
+tooltip = "Antistasi LS/3AS Star Wars Extender ";
 actionName = "GitHub";
 action = "https://github.com/HakonRydland/A3AExtender";
